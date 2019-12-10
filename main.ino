9d38308fc3488a805742705e812fe2baff37fbee
selligZeittom <gilmottiez@gmail.com>
2019-12-10 17:50:44 +0100

    CHG : updated characteristics from byte to float

--- main.ino
+++ main.ino
@@ -6,43 +6,11 @@
 /* Include library for BLE */
 #include <ArduinoBLE.h>
 
-/* Define some values for bit reduction */
-#define MAX_VIBRATION_VALUE (4.0f)
-#define MIN_VIBRATION_VALUE (0.0f)
-#define DIVIDOR ((float)(MAX_VIBRATION_VALUE - MIN_VIBRATION_VALUE))
-#define FACTOR ((float)(0xff))
-
 /* Create the variables for the BLE exchanges */
 BLEService accelerometerService("19B10010-E8F2-537E-4F6C-D104768A1214");
-BLEByteCharacteristic accelerometerCharacteristicX("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-BLEByteCharacteristic accelerometerCharacteristicY("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-BLEByteCharacteristic accelerometerCharacteristicZ("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-
-
-/*
-  Performs a normalization of the acceleration to save memory
-  -2.0 -> 0
-  +2.0 -> 255
-*/
-uint8_t normalize_acceleration(float fAcc)
-{
-  /* Put an offset to the value */
-  float temp = fAcc + 2.0f;
-
-  /* Check for limits */
-  if(temp > MAX_VIBRATION_VALUE)
-  {
-    temp = MAX_VIBRATION_VALUE;
-  }
-  else if (temp < MIN_VIBRATION_VALUE)
-  {
-    temp = MIN_VIBRATION_VALUE;
-  }
-  temp *= FACTOR;
-  temp /= DIVIDOR;
-  uint8_t ret_val = (uint8_t)(temp);
-  return ret_val;
-}
+BLEFloatCharacteristic accelerometerCharacteristicX("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicY("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicZ("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
 
 void setup() 
 {
@@ -60,12 +28,12 @@ void setup()
   /* Initialize BLE */
   if (!BLE.begin()) 
   {
-    Serial.println("starting BLE failed!");
+    Serial.println("Failed to initialize BLE...");
     while (1);
   }
   
   /* Set the name of the device and the service */
-  BLE.setLocalName("AccelerometerNano33");
+  BLE.setLocalName("Nano33");
   BLE.setAdvertisedService(accelerometerService);
   
   /* Add the characteristics to the service */
@@ -73,14 +41,13 @@ void setup()
   accelerometerService.addCharacteristic(accelerometerCharacteristicY);
   accelerometerService.addCharacteristic(accelerometerCharacteristicZ);
 
-  
   /* Add the service */
   BLE.addService(accelerometerService);
 
   /* Set an initial value */
-  accelerometerCharacteristicX.writeValue(0);
-  accelerometerCharacteristicY.writeValue(0);
-  accelerometerCharacteristicZ.writeValue(0);
+  accelerometerCharacteristicX.writeValue(0.0f);
+  accelerometerCharacteristicY.writeValue(0.0f);
+  accelerometerCharacteristicZ.writeValue(0.0f);
   
   /* Start advertising */
   BLE.advertise();
@@ -94,25 +61,13 @@ void loop()
   
   /* Get the accelerations */
   float x, y, z;
-  uint8_t bX, bY, bZ;
-  bX = bY = bZ = 0;
-
   if (IMU.accelerationAvailable()) 
   {
     IMU.readAcceleration(x, y, z);
-    bX = normalize_acceleration(x);
-    bY = normalize_acceleration(y);
-    bZ = normalize_acceleration(z);
-    Serial.print("X : ");
-    Serial.print(bX);
-    Serial.print(", Y : ");
-    Serial.print(bY);
-    Serial.print(", Z : ");
-    Serial.println(bZ);
     
     /* Update the characteristics */
-    accelerometerCharacteristicX.writeValue(bX);
-    accelerometerCharacteristicY.writeValue(bY);
-    accelerometerCharacteristicZ.writeValue(bZ);
+    accelerometerCharacteristicX.writeValue(x);
+    accelerometerCharacteristicY.writeValue(y);
+    accelerometerCharacteristicZ.writeValue(z);
   }
 }
